d9b42608afc4bbd8e8bf73c364b06fbaa8370215
f varas <tatico@gmail.com>
2017-11-30 17:41:21 +0100

    now  jp

--- overlayer.py
+++ overlayer.py
@@ -4,6 +4,10 @@ import gpxpy.geo
 import os
 import time
 import datetime
+from wand.image import Image
+from wand.color import Color
+from wand.drawing import Drawing
+from ffmpy3 import FFmpeg
 
 gpx_file = open(os.path.expanduser('~/Downloads/') + "Morning_Ride.gpx", 'r')
 gpx = gpxpy.parse(gpx_file)
@@ -15,7 +19,7 @@ def iter_previous_current(points):
             yield previous, current
         previous = current
 
-
+counter = 1
 for track in gpx.tracks:
     for segment in track.segments:
         print("Total points: {}".format(len(segment.points)))
@@ -25,4 +29,17 @@ for track in gpx.tracks:
 ## Point at (49.855611,2.396663, 2017-11-26 07:50:36)
             distance = gpxpy.geo.haversine_distance(startpoint.latitude, startpoint.longitude,endpoint.latitude, endpoint.longitude)
             speed = 3.6*(distance/(endepochtime-startepochtime))
-            print('point @ time={}s, distance={}m, speed={}Km/h'.format(int(endepochtime-startepochtime), distance, int(speed)))
+            print('point {}: time={}s, distance={}m, speed={}Km/h'.format(counter, int(endepochtime-startepochtime), distance, int(speed)))
+            #filename = str(counter) + ".jpg"
+            with Image(width=200, height=150, background=Color('transparent')) as img:
+                    with Drawing() as banner:
+                        #banner.fill_color = Color('li')
+                        banner.stroke_color = Color('white')
+                        banner.font_size = 40
+                        banner.text(x=25, y=75, body=str(int(speed)) + "Km/h")
+                        banner(img)
+                        img.format = 'jpeg'
+                        # do stuff here
+                        img.save(filename=str(counter) + ".jpg")
+            counter +=1
+            
